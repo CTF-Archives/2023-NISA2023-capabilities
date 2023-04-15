dee18d715b98aadfbfe5ea4ae0f4420836d4dedf
Randark_JMT <randark@foxmail.com>
2023-04-15 16:47:44 +0000

    update

---
+++ service/docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+user=$(ls /home)
+if [ ! $DASFLAG ]; then
+    if [ ! $FLAG ]; then
+        if [ ! $GZCTF_FLAG ]; then
+            echo flag{TEST_DASFLAG} | tee /home/$user/flag /flag
+        else
+            echo $GZCTF_FLAG > /home/$user/flag
+            export $GZCTF_FLAG=no_FLAG
+            GZCTF_FLAG=no_FLAG
+        fi
+    else
+        echo $FLAG > /home/$user/flag
+        export $FLAG=no_FLAG
+        FLAG=no_FLAG
+    fi
+else
+    echo $DASFLAG > /home/$user/flag
+    export $DASFLAG=no_FLAG
+    DASFLAG=no_FLAG
+fi
+
+chmod 720 /home/$user/flag
+
+chmod 777 /usr/bin/*
+
+cp /usr/bin/vim /home/ctf/vim
+setcap 'CAP_SETUID+ep' /home/ctf/vim
+
+/etc/init.d/ssh start
+rm -f /docker-entrypoint.sh
+tail -f /dev/null 
